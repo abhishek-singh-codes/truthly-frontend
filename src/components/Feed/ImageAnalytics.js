1512f82e8c8b2d85f075fbbccb5889736dc9392e
Abhishek Singh <abhishek.singh@eroute.in>
2026-01-08 13:03:07 +0530

    Like correctly showing

--- src/components/Feed/ImageAnalytics.js
+++ src/components/Feed/ImageAnalytics.js
@@ -1,10 +1,14 @@
-import { FaHeart, FaRegComment } from "react-icons/fa";
+import { FaHeart, FaRegHeart, FaRegComment } from "react-icons/fa";
 
 const ImageAnalytics = ({ analytics }) => {
   return (
     <div className="flex text-xl justify-evenly">
       <button className="flex items-center gap-1">
-        <FaHeart />
+        {analytics.isLike ? (
+          <FaHeart className="text-red-500" />
+        ) : (
+          <FaRegHeart className="text-gray-500" />
+        )}
         <span>{analytics.like}</span>
       </button>
 
