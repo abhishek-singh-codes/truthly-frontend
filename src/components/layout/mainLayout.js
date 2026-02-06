25628fb6c79a55962f075c38c195a231152780da
abhishek-singh-codes <asingh19082000@gmail.com>
2026-02-06 12:23:42 +0530

    Working css

--- src/components/layout/mainLayout.js
+++ src/components/layout/mainLayout.js
@@ -5,33 +5,34 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      
-      {/* Navbar */}
-      <Navbar />
+    <div className="h-screen bg-gray-50">
+      {/* NAVBAR (fixed top) */}
+      <div className="fixed top-0 left-0 w-full z-50">
+        <Navbar />
+      </div>
 
-      {/* Main content */}
-      <div className="flex w-full gap-4 px-4 pt-4">
-
-        {/* LEFT SIDEBAR */}
-        <div className="hidden sm:block w-auto">
-          <div className="sticky border border-blue-200 rounded-md shadow-md p-4 w-full">
+      {/* MAIN LAYOUT */}
+      <div className="pt-16 flex">
+        {/* LEFT SIDEBAR (fixed) */}
+        <div className="hidden sm:block">
+          <div className="fixed top-16 left-4 w-64 border border-blue-200 rounded-md shadow-md p-4 bg-white">
             <Sidebar />
           </div>
         </div>
 
-        {/* MIDDLE CONTENT (WINDOW SCROLLS) */}
-        <div className="flex-1 border border-blue-200 rounded-md p-4">
-          <Outlet />
-        </div>
-
-        {/* RIGHT PANEL */}
-        <div className="hidden sm:block w-64">
-          <div className="sticky border border-blue-200 rounded-md shadow-md p-4">
-            <Distance />
+        {/* CENTER FEED (scrollable) */}
+        <div className="flex-1 px-4 ml-0 sm:ml-72 mr-0 sm:mr-72 h-[calc(100vh-4rem)]">
+          <div className="border border-blue-200 rounded-md p-4 bg-white">
+            <Outlet />
           </div>
         </div>
 
+        {/* RIGHT PANEL (fixed) */}
+        <div className="hidden sm:block">
+          <div className="fixed top-16 right-4 w-64 border border-blue-200 rounded-md shadow-md p-4 bg-white">
+            <Distance />
+          </div>
+        </div>
       </div>
     </div>
   );
