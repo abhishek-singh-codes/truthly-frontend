dfd72bee5d423f02eb3d1ffbf4e3d3552e027b24
abhishek-singh-codes <asingh19082000@gmail.com>
2026-02-10 22:27:45 +0530

    Added profile page

---
+++ src/components/user/Profile.js
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    {/* image */}
+                </div>
+                <div>
+                    {/* Bio */}
+                </div>
+                <div>
+                    {/* neighbors */}
+                </div>
+            </div>
+            <div>
+                <div>
+                    {/* Images */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
